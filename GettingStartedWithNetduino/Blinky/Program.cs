c0fd46b7bc5de2f1ca987decd1d91a1d1fe477dd
Kyle Parrigan <kyle.parrigan@gmail.com>
2015-02-11 20:48:09 -0500

    Getting Started "First Projects" chapter.

---
+++ GettingStartedWithNetduino/Blinky/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace Blinky
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            const int sleep = 250;
+            var led = new OutputPort(Pins.ONBOARD_LED, false);
+
+            while(true)
+            {
+                led.Write(true);
+                Thread.Sleep(sleep);
+                led.Write(false);
+                Thread.Sleep(sleep);
+            }
+
+        }
+
+    }
+}
---
+++ GettingStartedWithNetduino/PushButton1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace PushButton1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var led = new OutputPort(Pins.ONBOARD_LED, false);
+            var button = new InputPort(Pins.ONBOARD_BTN, false, Port.ResistorMode.Disabled);
+            var buttonState = false;
+
+            while(true)
+            {
+                buttonState = button.Read();
+                led.Write(buttonState);
+            }
+        }
+
+    }
+}
