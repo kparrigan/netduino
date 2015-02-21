6e345c90d31aab8680185e231ce90ed2f39a95d7
Kyle Parrigan <kyle.parrigan@gmail.com>
2015-02-21 16:17:48 -0500

    Servo example.

---
+++ GettingStartedWithNetduino/SingleServo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace SingleServo
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            using(var servo = new PWM(PWMChannels.PWM_PIN_D5, 20000, 1500, PWM.ScaleFactor.Microseconds, false))
+            {
+                servo.Start();       
+
+                servo.Duration = 5000; // Full Right            
+                Thread.Sleep(500);
+                servo.Duration = 1000; // Full Left            
+                Thread.Sleep(500);
+                servo.Duration = 2500; // Full Center            
+                Thread.Sleep(500);
+
+                servo.Stop();
+            }            
+        }
+
+    }
+}
